c5bda7d6cc7f09e9ff45747f5a79573df4f6b469
duonghue2 <honghue2911999@gmail.com>
2020-10-18 06:03:19 +0700

    dddd

--- src/container/Categories/Categories.js
+++ src/container/Categories/Categories.js
@@ -3,13 +3,22 @@ import cssClasses from "./Categories.module.css";
 import Container from "../../components/UI/Container/Container";
 import { Divider } from "antd";
 import Course from "../../components/Course/Course";
+import dataService from "../../services/data.service";
 
 class Categories extends React.Component {
   state = {
-    // coursesData: {
-    // }
+    data: [],
   };
-  componentDidMount() {}
+  componentDidMount() {
+    dataService
+      .getListCategory()
+      .then((re) => {
+        this.setState({
+          data: re.data.categories,
+        });
+      })
+      .catch((e) => {});
+  }
 
   render() {
     return (
@@ -18,71 +27,35 @@ class Categories extends React.Component {
           <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
             Categories
           </h1>
-          <Divider
-            orientation="left"
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              width: "calc(50% - 45px)",
-              color: "#4f566b",
-              margin: "50px 0",
-            }}
-          >
-            Artificial Intelligence
-          </Divider>
-          <Course data={this.state.coursesData} />
-          <Divider
-            orientation="left"
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              width: "calc(50% - 45px)",
-              color: "#4f566b",
-              margin: "50px 0",
-            }}
-          >
-            Computer Science
-          </Divider>
-          <Course data={this.state.coursesData} />
-          <Divider
-            orientation="left"
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              width: "calc(50% - 45px)",
-              color: "#4f566b",
-              margin: "50px 0",
-            }}
-          >
-            Game Developer
-          </Divider>
-          <Course data={this.state.coursesData} />
-          <Divider
-            orientation="left"
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              width: "calc(50% - 45px)",
-              color: "#4f566b",
-              margin: "50px 0",
-            }}
-          >
-            Security
-          </Divider>
-          <Course data={this.state.coursesData} />
-          <Divider
-            orientation="left"
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              width: "calc(50% - 45px)",
-              color: "#4f566b",
-              margin: "50px 0",
-            }}
-          >
-            Web Developer
-          </Divider>
-          <Course data={this.state.coursesData} />
+          {Object.keys(this.state.data).map((data, index) => (
+            <div>
+              <Divider
+                orientation="left"
+                style={{
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                  width: "calc(50% - 45px)",
+                  color: "#4f566b",
+                  margin: "50px 0",
+                }}
+              >
+                {data}
+                {console.log(this.state.data[data])}
+              </Divider>
+              {this.state.data[data].map((data1, index1) => {
+                let t = [];
+                t.push(data1);
+                return (
+                  <Course
+                    data={t}
+                    logo={data1.organizationLogo}
+                    organizationName={data1.organizationName}
+                    id={data1.organizationId}
+                  />
+                );
+              })}
+            </div>
+          ))}
         </Container>
       </React.Fragment>
     );
