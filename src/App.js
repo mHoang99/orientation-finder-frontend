b1315b55dfb0f6a28ae8211b7bdf1365a5e8e1f8
duonghue2 <honghue2911999@gmail.com>
2020-10-17 09:38:38 +0700

    add modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Layout } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css';
-import Auth from './container/Auth/Auth';
-import Landing from './container/Landing/Landing'
-
-
+import React from "react";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+import Auth from "./container/Auth/Auth";
+import Landing from "./container/Landing/Landing";
+import ShowResult from "./components/ModalDetail/ShowResult";
+import Quiz from "./components/ModalDetail/ModalDoQuiz";
 const App = () => {
-    return (
-        <div>
-            <Layout style={{backgroundColor:"white"}}>
+  return (
+    <div>
+      <Quiz />
+      {/* <ShowResult /> */}
+      {/* <Layout style={{backgroundColor:"white"}}>
                 <Landing/>
                 <Auth />
-            </Layout>
-        </div>
-    );
+            </Layout> */}
+    </div>
+  );
 };
 
 export default App;
