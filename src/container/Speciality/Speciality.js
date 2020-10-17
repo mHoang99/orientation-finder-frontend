6c926df589b5500af37615e3017878e461ee6a30
Minh Hoang <vmhoang1999@gmail.com>
2020-10-17 12:33:34 +0700

    update Speciality

--- src/container/Speciality/Speciality.js
+++ src/container/Speciality/Speciality.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 import SalaryTable from '../../components/Table/SalaryTable/SalaryTable';
 import FeeAndScore from '../../components/Table/FeeAndScoreTable/FeeAndScore';
-import { Col, Row } from 'antd';
+import { Col, Row, Progress, Card } from 'antd';
 import cssClasses from './Speciality.module.css'
-
+import './Speciality.css'
 
 class Speciality extends React.Component {
     state = {
@@ -15,15 +16,39 @@ class Speciality extends React.Component {
 
         return (
             <React.Fragment>
-                <Row className={cssClasses.container} justify='space-around'>
-                    <Col span={12}>
-                        <SalaryTable />
-                    </Col>
-                    <Col span={12}>
-                        <FeeAndScore />
-                    </Col>
-                </Row>
-
+                <div className={cssClasses.Container} >
+                    <Row justify="center">
+                        <Card style={{border: "none"}}>
+                            <Row style={{ paddingBottom: "30px" }}>
+                                <Col align="end" style={{ padding: "30px" }}>
+                                    <h1 className={cssClasses.Title}>ARTIFICIAL INTELLIGENT</h1>
+                                    <h3 className={cssClasses.Compatible}>Compatible Level</h3>
+                                </Col>
+                                <Col>
+                                    <Progress
+                                        type="circle"
+                                        strokeColor={{
+                                            '0%': '#108ee9',
+                                            '100%': '#87d068',
+                                        }}
+                                        percent={60}
+                                    />
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Row>
+                    <Row justify="center">
+                        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' style={{width: '100%'}}/>
+                    </Row>
+                    <Row justify='space-around'>
+                        <Col span={12}>
+                            <SalaryTable />
+                        </Col>
+                        <Col span={12}>
+                            <FeeAndScore />
+                        </Col>
+                    </Row>
+                </div>
             </React.Fragment>
         )
     }
